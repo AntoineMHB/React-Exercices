7d9b8adc5051a1b99088f4dd24e1cbfa4c508a70
AntoineMHB <antoinemuhumbania@gmail.com>
2025-08-08 11:41:20 +0200

    Add formatting on the entered number

--- src/components/Calculator.jsx
+++ src/components/Calculator.jsx
@@ -4,12 +4,23 @@ export default function() {
     const [clickedText, setClickedText] = useState('')
 
     function handleClick(event) {
-        setClickedText(event.target.clickedText)
+      if (clickedText.length < 9) {
+        setClickedText(prev => prev + event.target.textContent)
+      }
+        
+    }
+
+    function formatWithCommas(numberString) {
+      if (!numberString) return "";
+      return parseInt(numberString).toLocaleString("en-US");
+
     }
     return (
         <>
-          <h1>{clickedText}</h1>
-          <div className="flex items-center justify-center gap-2">
+          <div className="w-[250px] h-[50px] bg-gray-800">
+             <p className="w-full text-4xl text-white tracking-widest">{formatWithCommas(clickedText)}</p>
+          </div>
+          <div className="pt-2 flex items-center justify-center gap-2">
             <button className="w-14 h-14 rounded-full bg-black">AC</button>
             <button className="w-14 h-14 rounded-full bg-black">+/-</button>
             <button className="w-14 h-14 rounded-full bg-black">%</button>
@@ -17,29 +28,29 @@ export default function() {
           </div>
          
          <div className="pt-2 flex items-center justify-center gap-2">
-            <button className="w-14 h-14 rounded-full bg-black">7</button>
-            <button className="w-14 h-14 rounded-full bg-black">8</button>
-            <button className="w-14 h-14 rounded-full bg-black">9</button>
+            <button onClick={handleClick} className="w-14 h-14 rounded-full bg-black">7</button>
+            <button onClick={handleClick} className="w-14 h-14 rounded-full bg-black">8</button>
+            <button onClick={handleClick} className="w-14 h-14 rounded-full bg-black">9</button>
             <button className="w-14 h-14 rounded-full bg-amber-500">x</button>
           </div>
 
          <div className="pt-2 flex items-center justify-center gap-2">
-            <button className="w-14 h-14 rounded-full bg-black">4</button>
-            <button className="w-14 h-14 rounded-full bg-black">5</button>
-            <button className="w-14 h-14 rounded-full bg-black">6</button>
+            <button onClick={handleClick} className="w-14 h-14 rounded-full bg-black">4</button>
+            <button onClick={handleClick} className="w-14 h-14 rounded-full bg-black">5</button>
+            <button onClick={handleClick} className="w-14 h-14 rounded-full bg-black">6</button>
             <button className="w-14 h-14 rounded-full bg-amber-500">-</button>
           </div>
 
          <div className="pt-2 flex items-center justify-center gap-2">
-            <button className="w-14 h-14 rounded-full bg-black">1</button>
-            <button className="w-14 h-14 rounded-full bg-black">2</button>
-            <button className="w-14 h-14 rounded-full bg-black">3</button>
+            <button onClick={handleClick} className="w-14 h-14 rounded-full bg-black">1</button>
+            <button onClick={handleClick} className="w-14 h-14 rounded-full bg-black">2</button>
+            <button onClick={handleClick} className="w-14 h-14 rounded-full bg-black">3</button>
             <button className="w-14 h-14 rounded-full bg-amber-500">+</button>
           </div>
 
          <div className="pt-2 flex items-center justify-center gap-2">
-            <button className="w-[120px] h-14 bg-black rounded-full">0</button>
-            <button className="w-14 h-14 rounded-full bg-black">.</button>
+            <button onClick={handleClick} className="w-[120px] h-14 bg-black rounded-full">0</button>
+            <button onClick={handleClick} className="w-14 h-14 rounded-full bg-black">.</button>
             <button className="w-14 h-14 rounded-full bg-amber-500">=</button>
           </div>
 
