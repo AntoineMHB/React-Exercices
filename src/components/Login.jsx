26947d9e597ed07ca267593d62de80917c6710e9
AntoineMHB <antoinemuhumbania@gmail.com>
2025-08-28 14:14:50 +0200

    Change the user state

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,4 +1,9 @@
+import { useDispatch } from "react-redux";
+import { login } from "../features/user.jsx";
+
 export default function Login() {
+  const dispatch = useDispatch();
+
   return (
     <div>
       <button className="bg-amber-200 rounded-full rounded-2 w-[70px]">
--- src/features/user.jsx
+++ src/features/user.jsx
@@ -2,12 +2,16 @@ import { createSlice } from "@reduxjs/toolkit";
 
 export const userSlice = createSlice({
   name: "user",
-  initialState: { value: { name: "", age: 0, email: "" } },
+  initialState: {
+    value: { name: "Antoine", age: 0, email: "antoine@gmail.com" },
+  },
   reducers: {
     login: (state, action) => {
       state.value = action.payload;
     },
   },
 });
+
+export const { login } = userSlice.actions;
 
 export default userSlice.reducer;
